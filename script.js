c8d3cd1ad3943fef83e66712b129392fd00f50a2
iicoats <145803389+iicoats@users.noreply.github.com>
2023-12-12 20:59:58 -0600

    working on form and found some pictures from unsplash

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 "use strict";
 
-num1 = document.getElementById("0.65").value;
-num2 = document.getElementById("miles").value;
+// from chatgpt with adjustments
+let num1, num2;
+
+function multiplyBy() {
+  
+  num1 = parseFloat(document.getElementById("perMile").value);
+  num2 = parseFloat(document.getElementById("miles").value);
+  
+  
+  if (isNaN(num1) * isNaN(num2)) {
+    alert("Please enter valid numbers");
+    return;
+  }
+  
+  
+  document.getElementById("mileage").innerHTML = num1 * num2;
+}
+
