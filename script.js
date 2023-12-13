69ce728600a97ff3d87e4d7878d06dbf9019bd11
iicoats <145803389+iicoats@users.noreply.github.com>
2023-12-12 21:11:54 -0600

    got a working form with chatgpt and added it to pages

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 "use strict";
 
-// from chatgpt with adjustments
-let num1, num2;
 
-function multiplyBy() {
-  
-  num1 = parseFloat(document.getElementById("perMile").value);
-  num2 = parseFloat(document.getElementById("miles").value);
-  
-  
-  if (isNaN(num1) * isNaN(num2)) {
-    alert("Please enter valid numbers");
-    return;
-  }
-  
-  
-  document.getElementById("mileage").innerHTML = num1 * num2;
-}
 
